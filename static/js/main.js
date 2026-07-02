e9ec54f059f67c9b647c20176a39c6af5253d80a
Caleb Denio <cjdenio44@gmail.com>
2026-07-02 12:47:17 -0600

    default bearing to 0

--- static/js/main.js
+++ static/js/main.js
@@ -142,7 +142,7 @@ function renderVehicleIcon(vehicle) {
     }" style="--color: #${vehicle.route.color};">
               <div style="position: absolute; inset: 0; display: flex; align-items: center; justify-content: center;">
                 <svg style="transform: rotate(${
-                  vehicle.bearing
+                  vehicle.bearing ?? 0
                 }deg) translateY(-12px); width: 18px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M300.3 199.2C312.9 188.9 331.4 189.7 343.1 201.4L471.1 329.4C480.3 338.6 483 352.3 478 364.3C473 376.3 461.4 384 448.5 384L192.5 384C179.6 384 167.9 376.2 162.9 364.2C157.9 352.2 160.7 338.5 169.9 329.4L297.9 201.4L300.3 199.2z"/></svg>
               </div>
               ${
